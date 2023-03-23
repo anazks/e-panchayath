54ccddbaad2500f5890dfa739eb3cddd39d1ad20
ZionIt-Tech-Team <zionitedu@gmail.com>
2023-03-23 17:02:00 +0530

    logins post

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs =require('hbs');
-
+const con = require('./config/Config')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session')
 
 var app = express();
 
@@ -20,7 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge:600000 }
+}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var con = require('../config/Config')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -12,7 +12,14 @@ router.get('/Login', function(req, res, next) {
   res.render('user/Login');
 });
 router.get('/Home', function(req, res, next) {
-  res.render('user/Home');
+    if(req.session.user){
+      let user = req.session.user;
+      console.log(req.session.user)
+      res.render('user/Home',{user})
+    }else{
+      res.render('user/Home');
+    }
+ 
 });
 router.get('/update-user', function(req, res, next) {
   res.render('user/updateProfile');
@@ -23,4 +30,36 @@ router.get('/complaint-Register', function(req, res, next) {
 router.get('/news-feeds', function(req, res, next) {
   res.render('user/newsFeeds');
 });
+router.get('/notifications', function(req, res, next) {
+  res.render('user/Notification');
+});
+router.post('/Register',(req,res)=>{
+  var sql ="insert into user set ?"
+    console.log(req.body)
+    con.query(sql,req.body,(err,row)=>{
+      if(err){
+        console.log(err)
+      }else{
+        console.log("data inserted")
+        res.redirect('/Login')
+      }
+    })
+})
+router.post('/Login',(req,res)=>{
+  var sql ="select * from user where email = ? and password = ?"
+  let {email} = req.body;
+  let {password} = req.body;
+  con.query(sql,[email,password],(err,row)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(row.length>0){
+        req.session.user=row[0];
+        res.redirect('/Home')
+      }else{
+          res.redirect('/Login')
+      }
+    }
+  })
+})
 module.exports = router;
